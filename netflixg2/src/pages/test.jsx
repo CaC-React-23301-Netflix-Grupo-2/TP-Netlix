8a5dc49e317e5c7c3c01e72f71e77398b47af3fd
David Barraud <davidbarraud@gmail.com>
2023-05-14 21:38:09 -0300

    rama David cambios en el index

--- netflixg2/src/pages/test.jsx
+++ netflixg2/src/pages/test.jsx
@@ -2,14 +2,24 @@ import Header from '@/components/Header'
 import BillboardBox from '@/components/BillboardBox'
 import Footer from '@/components/Footer'
 import styles from '../styles/Test.module.css'
+import AccordionComp from '../components/AccordionComp'
+import FormEmail from '../components/FormEmail';
 
 const Test = () => {
     return (
+        <>
         <div className={`${styles.test}`}>
             <Header />
             <BillboardBox />
-            <Footer />
+                        
         </div>
+        <div className={`${styles.test2}`} >
+         <AccordionComp />
+         <FormEmail />
+        <Footer />
+        </div>
+        </>
+
     )
 }
 
